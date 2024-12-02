8199311029a0a5d9a73241a0647cfc5634f82171
mquaan <nmquan.se@gmail.com>
2024-12-02 19:29:32 +0700

    edit env

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 //   baseURL: 'http://localhost:8080/api'
-    baseURL: import.meta.env.VITE_BACKEND_URL
+    baseURL: import.meta.env.VITE_BACKEND_URL + '/api'
 })
 
 export default axiosInstance;
