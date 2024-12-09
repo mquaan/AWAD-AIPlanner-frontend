e447a1404ef65aa9e756f53339b58719ef151904
mquaan <nmquan.se@gmail.com>
2024-12-09 19:51:48 +0700

    Edit board

--- config/axiosConfig.js
+++ config/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:8080/api'
     baseURL: import.meta.env.VITE_BACKEND_URL + '/api'
 })
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './index.css';
@@ -7,6 +6,7 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
+import Board from './pages/Board';
 
 import PrivateRoute from './PrivateRoute';
 
@@ -19,6 +19,8 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/account' element={<Account />} />
+            <Route path="/board" element={<Board />} />
+            <Route path="/board/:id" element={<Board />} />
           </Route>
         </Route>
         <Route path="/login" element={<Login />} />
